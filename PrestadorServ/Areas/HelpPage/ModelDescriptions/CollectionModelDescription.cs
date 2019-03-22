8bfc964d344cfb65ef8211018e52d4cb6a7369b9
igor.rodrigues <igor.rodrigues@amcom.com.br>
2019-03-22 11:58:06 -0300

    Start project

---
+++ PrestadorServ/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PrestadorServ.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ PrestadorServ/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PrestadorServ.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
