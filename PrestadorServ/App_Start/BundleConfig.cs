c9d0fcec68eb5eae641a8db3dfdb859aff6cd4e1
igor.rodrigues <igor.rodrigues@amcom.com.br>
2019-04-02 16:21:23 -0300

    BUndle config

--- PrestadorServ/App_Start/BundleConfig.cs
+++ PrestadorServ/App_Start/BundleConfig.cs
@@ -8,20 +8,30 @@ namespace PrestadorServ
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/style/angular").Include("~/Content/PrestadorServClient/styles.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Content/PrestadorServClient/runtime.*",
+                "~/Content/PrestadorServClient/es2015-*",
+                "~/Content/PrestadorServClient/polyfills.*",
+                "~/Content/PrestadorServClient/scripts.*",
+                "~/Content/PrestadorServClient/main.*"
+                ));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
         }
     }
 }
